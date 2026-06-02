c28d0ba477cb2fbe7c7e9e949671e86624114762
Levi putra perdana <putralevi29@gmail.com>
2026-06-02 08:50:23 +0700

    Add Guru class to display Majalah details

---
+++ Tugas_Modul 5/Levi_090/package anggota/Guru.java
@@ -0,0 +1,14 @@
+package anggota;
+
+import admin.Majalah;
+
+public class Guru {
+
+    public static void main(String[] args) {
+
+        Majalah m = new Majalah();
+
+        System.out.println(m.namaMajalah);
+        m.tampilMajalah();
+    }
+}
