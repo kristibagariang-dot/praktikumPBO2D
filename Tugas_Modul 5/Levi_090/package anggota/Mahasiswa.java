9893af406d522736ac4d56aa040121214ac6b7f3
Levi putra perdana <putralevi29@gmail.com>
2026-06-02 08:50:48 +0700

    Add Mahasiswa class to display Komik details

---
+++ Tugas_Modul 5/Levi_090/package anggota/Mahasiswa.java
@@ -0,0 +1,14 @@
+package anggota;
+
+import admin.Komik;
+
+public class Mahasiswa {
+
+    public static void main(String[] args) {
+
+        Komik k = new Komik();
+
+        System.out.println(k.namaKomik);
+        k.tampilKomik();
+    }
+} 
