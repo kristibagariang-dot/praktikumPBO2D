c699a59cb37e597f78f9f62d92ab5f4c5ec06cf5
sittiarika17-ui <sittiarika17@gmail.com>
2026-06-02 23:19:40 +0700

    Update Pasien.java

--- Tugas_Modul 5/Arika_087/Pasien.java
+++ Tugas_Modul 5/Arika_087/Pasien.java
@@ -1,1 +1,37 @@
-yy
+package aricha;
+
+import pasien.DataPasien;
+import pasien.RekamMedis;
+import pelayanan.Pemeriksaan;
+import pelayanan.Obat;
+
+public class Pasien extends DataPasien {
+
+    // final
+    public final String rumahSakit = "RS Sehat Sentosa";
+
+    public Pasien(String nama, int umur) {
+        super(nama, umur);
+    }
+
+    public void tampilPasien() {
+
+        System.out.println("=== DATA PASIEN ===");
+
+        infoPasien();
+
+        System.out.println("Rumah Sakit : " + rumahSakit);
+    }
+
+    public void periksa() {
+
+        Pemeriksaan p = new Pemeriksaan();
+        RekamMedis rm = new RekamMedis();
+        Obat o = new Obat();
+
+        System.out.println();
+        rm.tampilRM();
+        p.tampilHasil();
+        o.tampilObat();
+    }
+}
