3a240e543d695791e745c9e129ad80e8b28d3711
sittiarika17-ui <sittiarika17@gmail.com>
2026-06-02 23:17:29 +0700

    Update Dokter.java

--- Tugas_Modul 5/Arika_087/Dokter.java
+++ Tugas_Modul 5/Arika_087/Dokter.java
@@ -1,1 +1,33 @@
-yy
+package aricha;
+
+public class Dokter {
+
+    // public
+    public String namaDokter;
+
+    // protected
+    protected String spesialis;
+
+    // private
+    private final String kodeDokter = "DK001";
+
+    public Dokter(String namaDokter, String spesialis) {
+        this.namaDokter = namaDokter;
+        this.spesialis = spesialis;
+    }
+
+    // public method
+    public void tampilDokter() {
+
+        System.out.println("=== DATA DOKTER ===");
+        System.out.println("Nama Dokter : " + namaDokter);
+        System.out.println("Spesialis   : " + spesialis);
+
+        tampilKode();
+    }
+
+    // private method
+    private void tampilKode() {
+        System.out.println("Kode Dokter : " + kodeDokter);
+    }
+}
