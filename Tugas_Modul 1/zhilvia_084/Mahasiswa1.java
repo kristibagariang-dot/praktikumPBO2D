68c982355ce71d59542db9d50cd55ec6fb91045f
zhilviaqutrunnadamk-droid <zhilviaqutrunnadamk@gmail.com>
2026-06-02 23:28:07 +0700

    Update Mahasiswa1.java

--- Tugas_Modul 1/zhilvia_084/Mahasiswa1.java
+++ Tugas_Modul 1/zhilvia_084/Mahasiswa1.java
@@ -1,1 +1,36 @@
-mahasiswa
+public class Mahasiswa {
+private String nim, nama, alamat;
+private int nilai ;
+public String getNama(){
+return nama;
+}
+public void setNama(String nama){
+this.nama = nama;
+}
+public String getNim(){
+return nim;
+}
+public void setNim(String nim){
+this.nim = nim;
+}
+public static void main(String[] args) {
+Mahasiswa m= new Mahasiswa(); m.setNim("250631100084");
+m.setNama("Zhilvia Qutrun Nada Mk");
+System.out.println("Nim :"+ m.getNim());
+System.out.println("Nama :"+ m.getNama());
+Mahasiswa mahasiswa1= new Mahasiswa();
+Mahasiswa mahasiswa2= new Mahasiswa();
+Mahasiswa mahasiswa3= new Mahasiswa();
+Scanner scan = new Scanner(System.in);
+System.out.print("Masukkan Nama Mahasiswa 1 : ");
+mahasiswa1.nama = scan.nextLine();
+System.out.print("Masukkan Nama Mahasiswa 2 : ");
+mahasiswa2.nama = scan.nextLine();
+System.out.print("Masukkan Nama Mahasiswa 3 : ");
+mahasiswa3.nama = scan.nextLine();
+System.out.println("BELAJAR CLASS DAN OBJEK");
+System.out.println(mahasiswa1.nama);
+System.out.println(mahasiswa2.nama);
+System.out.println(mahasiswa3.nama);
+}
+}
