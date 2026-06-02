7ab146b3f9e7a0879c51a40805272e950719537d
mahsunatulumamah32-art <mahsunatulumamah32@gmail.com>
2026-06-03 00:26:05 +0700

    Update Buku1.java

--- Tugas_Modul 2/Umamah_085/Buku1.java
+++ Tugas_Modul 2/Umamah_085/Buku1.java
@@ -1,1 +1,87 @@
-Main
+/*@author Taufiq*/
+class Buku {
+
+    String judul;
+    String pengarang;
+    String penerbit;
+    int tahunTerbit;
+    String kategoriBuku;
+    int stokBuku;
+
+    Buku() {
+    }
+    Buku(String judul, String pengarang) {
+        this.judul = judul;
+        this.pengarang = pengarang;
+    }
+
+    Buku(String judul, String pengarang, String penerbit,
+         int tahunTerbit, String kategoriBuku, int stokBuku) {
+
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.penerbit = penerbit;
+        this.tahunTerbit = tahunTerbit;
+        this.kategoriBuku = kategoriBuku;
+        this.stokBuku = stokBuku;
+    }
+    void tampilkanBuku() {
+        System.out.println("Judul Buku    : " + judul);
+        System.out.println("Pengarang     : " + pengarang);
+        System.out.println("Penerbit      : " + penerbit);
+        System.out.println("Tahun Terbit  : " + tahunTerbit);
+        System.out.println("Kategori Buku : " + kategoriBuku);
+        System.out.println("Stok Buku     : " + stokBuku);
+    }
+}
+class Novel {
+    String judul;
+    String penulis;
+    int tahunTerbit;
+    String kategoriNovel;
+    String statusPeminjaman;
+
+    Novel() {
+    }
+    Novel(String judul, String penulis) {
+        this.judul = judul;
+        this.penulis = penulis;
+    }
+    Novel(String judul, String penulis, int tahunTerbit,
+          String kategoriNovel, String statusPeminjaman) {
+
+        this.judul = judul;
+        this.penulis = penulis;
+        this.tahunTerbit = tahunTerbit;
+        this.kategoriNovel = kategoriNovel;
+        this.statusPeminjaman = statusPeminjaman;
+    }
+    void tampilkanNovel() {
+        System.out.println("Judul Novel       : " + judul);
+        System.out.println("Penulis           : " + penulis);
+        System.out.println("Tahun Terbit      : " + tahunTerbit);
+        System.out.println("Kategori Novel    : " + kategoriNovel);
+        System.out.println("Status Peminjaman : " + statusPeminjaman);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Buku buku1 = new Buku(
+                "Menjadi Lebih Baik",
+                "Mahsunatul Umamah",
+                "AdityaPurnama",
+                2003,
+                "Agama",
+                24);
+
+        buku1.tampilkanBuku();
+        System.out.println();
+        Novel novel1 = new Novel(
+                "akhlak",
+                "MahsunatulUmamah",
+                2004,
+                "Religius",
+                "Sosial");
+        novel1.tampilkanNovel();
+    }
+}
