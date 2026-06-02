0c81f584ffe8dd501c482c4626c11ca2467a286b
sittiarika17-ui <sittiarika17@gmail.com>
2026-06-02 23:25:14 +0700

    Update Obat.java

--- Tugas_Modul 5/Arika_087/Obat.java
+++ Tugas_Modul 5/Arika_087/Obat.java
@@ -1,1 +1,16 @@
-yy
+package pelayanan;
+
+public final class Obat {
+
+    // final
+    public final String namaObat = "Paracetamol";
+
+    // private
+    private int harga = 5000;
+
+    public void tampilObat() {
+
+        System.out.println("Obat         : " + namaObat);
+        System.out.println("Harga Obat   : " + harga);
+    }
+}
