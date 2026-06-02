07674ade0569f4ab3d57485ed271c88cffc9826b
Nurulqomariyah1 <qomariamifda@gmail.com>
2026-06-02 19:33:12 +0700

    Add files via upload

---
+++ Tugas_Modul 5/Riya_094/package dookter/JadwalDokter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dookter;
+
+// final class
+public final class JadwalDokter {
+
+    public void tampilJadwal() {
+        System.out.println("Jadwal Dokter : Senin - Jumat");
+    }
+}
