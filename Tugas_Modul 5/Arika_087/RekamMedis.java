baca6615a9ffeea63da981e4ea34d0e1142e2e19
sittiarika17-ui <sittiarika17@gmail.com>
2026-06-02 23:21:50 +0700

    Update RekamMedis.java

--- Tugas_Modul 5/Arika_087/RekamMedis.java
+++ Tugas_Modul 5/Arika_087/RekamMedis.java
@@ -1,1 +1,16 @@
-yy
+package pasien;
+
+public final class RekamMedis {
+
+    // final
+    public final String kodeRM = "RM001";
+
+    // private
+    private String penyakit = "Demam";
+
+    public void tampilRM() {
+
+        System.out.println("Kode RM     : " + kodeRM);
+        System.out.println("Penyakit    : " + penyakit);
+    }
+}
