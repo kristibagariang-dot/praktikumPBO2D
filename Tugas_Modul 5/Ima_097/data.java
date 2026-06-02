494ea190f31bb8a0f07d1921b55c3a0eff3ecdb4
Ima rotul hasanah <imarotulhasanah14@gmail.com>
2026-06-02 22:55:57 +0700

    Update data.java

--- Tugas_Modul 5/Ima_097/data.java
+++ Tugas_Modul 5/Ima_097/data.java
@@ -1,1 +1,18 @@
-ima
+package data;
+
+import siswa.Mahasiswa;
+
+public class Univ {
+    public static void main(String[] args) {
+     
+        Mahasiswa mhs1 = new Mahasiswa(
+                        "Imaa",
+                        19,
+                        "Informatika",
+                        "Bangkalan",
+                        "class D"
+        );
+        System.out.println(mhs1.tampilData());
+    }
+ 
+}
