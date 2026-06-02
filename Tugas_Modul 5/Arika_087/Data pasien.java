f1930f578ee02344d8c49dc05299e0ad74185f55
sittiarika17-ui <sittiarika17@gmail.com>
2026-06-02 23:20:41 +0700

    Update Data pasien.java

--- Tugas_Modul 5/Arika_087/Data pasien.java
+++ Tugas_Modul 5/Arika_087/Data pasien.java
@@ -1,1 +1,31 @@
-yy
+package pasien;
+
+public class DataPasien {
+
+    // public
+    public String nama;
+
+    // protected
+    protected int umur;
+
+    // private
+    private String alamat = "Pamekasan";
+
+    public DataPasien(String nama, int umur) {
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    public void infoPasien() {
+
+        System.out.println("Nama Pasien : " + nama);
+        System.out.println("Umur        : " + umur);
+
+        tampilAlamat();
+    }
+
+    // private method
+    private void tampilAlamat() {
+        System.out.println("Alamat      : " + alamat);
+    }
+}
