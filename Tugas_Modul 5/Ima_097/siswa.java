a04b52bbd65cbec925030ed501c3ddc6a516cdeb
Ima rotul hasanah <imarotulhasanah14@gmail.com>
2026-06-02 22:54:57 +0700

    Update siswa.java

--- Tugas_Modul 5/Ima_097/siswa.java
+++ Tugas_Modul 5/Ima_097/siswa.java
@@ -1,1 +1,19 @@
-imaa
+package siswa;
+
+ import orang.Dosen;
+ public class Mahasiswa extends Dosen {
+     
+     public String kelas;
+     public Mahasiswa(String nama,int umur,String matkul,String alamat,String kelas){
+         
+         super(nama,umur,matkul,alamat);
+         this.kelas = kelas;
+     }
+     public String tampilData(){//method
+         
+         return "Nama : " + nama +
+                "\nUmur : " + getUmur() +
+                "\nMatkul : " + matkul +
+                "\nkelas : " + kelas;
+     }
+}
