fedb563f932c5b7bfd8b50f801261cbe51916dbb
Nurulqomariyah1 <qomariamifda@gmail.com>
2026-06-02 19:22:27 +0700

    Add files via upload

--- Tugas_Modul 5/Package dookter/Dokter.java
+++ Tugas_Modul 5/Package dookter/Dokter.java
@@ -1,1 +1,23 @@
-bebas
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dookter;
+
+public class Dokter {
+
+    protected String namaDokter;
+
+    private String spesialis;
+
+    public Dokter(String namaDokter, String spesialis) {
+        this.namaDokter = namaDokter;
+        this.spesialis = spesialis;
+    }
+
+    public void tampilDokter() {
+        System.out.println("Nama Dokter : " + namaDokter);
+        System.out.println("Spesialis : " + spesialis);
+    }
+}
