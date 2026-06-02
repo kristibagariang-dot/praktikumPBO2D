b1812cb3d50d383eb78f3d199fffc7234158f128
sittiarika17-ui <sittiarika17@gmail.com>
2026-06-02 23:24:10 +0700

    Update Pemeriksaan.java

--- Tugas_Modul 5/Arika_087/Pemeriksaan.java
+++ Tugas_Modul 5/Arika_087/Pemeriksaan.java
@@ -1,1 +1,12 @@
-yy
+package pelayanan;
+
+public class Pemeriksaan {
+
+    // protected
+    protected String hasil = "Pasien dalam kondisi baik";
+
+    public void tampilHasil() {
+
+        System.out.println("Hasil Periksa : " + hasil);
+    }
+}
