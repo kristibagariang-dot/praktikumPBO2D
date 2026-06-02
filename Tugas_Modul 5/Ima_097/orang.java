2813b628e71c5096dcf1d1e850b5572d5a974544
Ima rotul hasanah <imarotulhasanah14@gmail.com>
2026-06-02 22:52:46 +0700

    Update orang.java

--- Tugas_Modul 5/Ima_097/orang.java
+++ Tugas_Modul 5/Ima_097/orang.java
@@ -1,1 +1,19 @@
-imaa
+package orang;
+
+public class Dosen {
+    
+ public String nama;
+ private int umur;
+ protected String matkul;
+ String alamat;
+ 
+ public Dosen(String nama, int umur, String matkul, String alamat){//untuk mengisi atribut otoomatis saat objekdibuat
+    this.nama = nama;
+    this.umur = umur;
+    this.matkul = matkul;
+    this.alamat = alamat;
+}
+ public int getUmur(){//get(agar class lain bisa melihat umur)
+     return umur;
+ }
+}
