8034a1a91dcb1c96357848c05df65018a686f53a
sittiarika17-ui <sittiarika17@gmail.com>
2026-06-02 23:14:14 +0700

    Update RumahSakit.java

--- Tugas_Modul 5/Arika_087/RumahSakit.java
+++ Tugas_Modul 5/Arika_087/RumahSakit.java
@@ -1,1 +1,17 @@
-ai
+package aricha;
+
+public class RumahSakit {
+
+    public static void main(String[] args) {
+
+        Dokter d1 = new Dokter("Dr. Budi", "Anak");
+        Pasien p1 = new Pasien("Salsa", 19);
+
+        d1.tampilDokter();
+
+        System.out.println();
+
+        p1.tampilPasien();
+        p1.periksa();
+    }
+}
