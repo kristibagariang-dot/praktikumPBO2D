96b6b5ce13bb91fe031a53c04e3a735de5b904d3
sittiarika17-ui <sittiarika17@gmail.com>
2026-06-02 22:45:05 +0700

    Update Mahasiswa3.java

--- Tugas_Modul 1/Arika_087/Mahasiswa3.java
+++ Tugas_Modul 1/Arika_087/Mahasiswa3.java
@@ -1,1 +1,48 @@
-icha
+import java.util.Scanner;
+
+public class Mahasiswa {
+
+    String nim;
+    String nama;
+    double nilai;
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlah = input.nextInt();
+        input.nextLine();
+
+        Mahasiswa[] data = new Mahasiswa[jumlah];
+
+        for (int i = 0; i < jumlah; i++) {
+
+            data[i] = new Mahasiswa();
+
+            System.out.println("\nData Mahasiswa ke-" + (i + 1));
+
+            System.out.print("NIM : ");
+            data[i].nim = input.nextLine();
+
+            System.out.print("Nama : ");
+            data[i].nama = input.nextLine();
+
+            System.out.print("Nilai : ");
+            data[i].nilai = input.nextDouble();
+            input.nextLine();
+        }
+
+        System.out.println("\n=== DATA MAHASISWA ===");
+
+        for (int i = 0; i < jumlah; i++) {
+
+            System.out.println("NIM   : " + data[i].nim);
+            System.out.println("Nama  : " + data[i].nama);
+            System.out.println("Nilai : " + data[i].nilai);
+            System.out.println("----------------------");
+        }
+
+        input.close();
+    }
+              }
