3e4ece09543b15760e2937aa9b3e27fd462ec1b1
Nurulqomariyah1 <qomariamifda@gmail.com>
2026-06-02 19:01:55 +0700

    Add files via upload

---
+++ Tugas_Modul 5/Riya_094/Login.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+public class Login {
+
+    private String username = "admin";
+
+    public void masuk() {
+        System.out.println("Login berhasil");
+        System.out.println("Username : " + username);
+    }
+}
