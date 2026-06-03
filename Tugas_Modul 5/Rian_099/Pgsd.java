f8bd53c5433b77a45b96aafeca59754ed189db29
nayansss161006-svg <nayansss161006@gmail.com>
2026-06-03 17:27:16 +0700

    Create Pgsd.java

---
+++ Tugas_Modul 5/Rian_099/Pgsd.java
@@ -0,0 +1,17 @@
+package prodi;
+
+import matkul.pai;
+
+public class pgsd extends pai {
+
+    public void tampilData() {
+
+        System.out.println("Dosen : " + dosen);
+
+        System.out.println("Ruangan : " + ruangan);
+
+        tampilPublic();
+        tampilProtected();
+
+    }
+}
