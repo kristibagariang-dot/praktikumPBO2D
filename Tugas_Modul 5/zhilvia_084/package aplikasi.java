9d85682ca82796d4b77cefe0eb629e929638a154
zhilviaqutrunnadamk-droid <zhilviaqutrunnadamk@gmail.com>
2026-06-03 14:19:14 +0700

    Update package aplikasi.java

--- Tugas_Modul 5/zhilvia_084/package aplikasi.java
+++ Tugas_Modul 5/zhilvia_084/package aplikasi.java
@@ -1,1 +1,13 @@
-py
+package aplikasi;
+
+import data.Mahasiswa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.tampilkanData();
+
+    }
+}
