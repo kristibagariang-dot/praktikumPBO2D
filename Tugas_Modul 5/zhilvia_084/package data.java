1fe598889844886fe437f85cf66067b3df775f33
zhilviaqutrunnadamk-droid <zhilviaqutrunnadamk@gmail.com>
2026-06-03 14:19:44 +0700

    Update package data.java

--- Tugas_Modul 5/zhilvia_084/package data.java
+++ Tugas_Modul 5/zhilvia_084/package data.java
@@ -1,1 +1,17 @@
-pl
+package data;
+
+import anggota.Orang;
+
+public class Mahasiswa extends Orang {
+
+    public void tampilkanData() {
+        System.out.println("Nama : " + nama);
+        System.out.println("Alamat : " + alamat);
+
+        // Tidak bisa akses private
+        // System.out.println(umur);
+
+        // Default juga tidak bisa beda package
+        // System.out.println(hobi);
+    }
+}
