04091ae03d447a91269aa6779a6178756d9f67c9
mahsunatulumamah32-art <mahsunatulumamah32@gmail.com>
2026-06-04 11:10:35 +0700

    Update MahasiswaAktif.java

--- Tugas_Modul 5/Umamah_085/MahasiswaAktif.java
+++ Tugas_Modul 5/Umamah_085/MahasiswaAktif.java
@@ -1,1 +1,17 @@
-Main
+ package kehadiran;
+
+/* @author Taufiq*/
+import data.Mahasiswa;
+
+public class MahasiswaAktif extends Mahasiswa {
+
+    public MahasiswaAktif(String nama, int umur, String jurusan, String alamat) {
+        super(nama, umur, jurusan, alamat);
+    }
+    public void aksesData() {
+        System.out.println("Akses dari subclass:");
+        System.out.println("Nama: " + nama);
+        System.out.println("Jurusan: " + jurusan);
+        System.out.println("Umur: " + getUmur());
+    }
+}
