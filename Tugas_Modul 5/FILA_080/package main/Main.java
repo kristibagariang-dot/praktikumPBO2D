df8160286d838b1e630d9d598c271fddabd7fe1a
himakafilatul-09 <kafilatulhima5@gmail.com>
2026-06-04 15:18:27 +0700

    Add files via upload

--- Tugas_Modul 5/FILA_080/package main/Main.java
+++ Tugas_Modul 5/FILA_080/package main/Main.java
@@ -1,1 +1,20 @@
-bebass
+package main;
+
+import hewan.Hewan;
+import pegawai.Pegawai;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Hewan h = new Hewan("Kucing", 2, 4.5);
+        Pegawai p = new Pegawai("Andi", "Programmer", 7000000);
+
+        System.out.println("=== DATA HEWAN ===");
+        h.tampilInfo();
+
+        System.out.println();
+
+        System.out.println("=== DATA PEGAWAI ===");
+        p.tampilInfo();
+    }
+}
