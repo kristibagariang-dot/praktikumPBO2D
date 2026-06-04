c28aef948b03229e522e076cf61c7f02ddcaa6e3
Yuyanikko <triwahyufajarmalik496@gmail.com>
2026-06-04 19:09:01 +0700

    Create Pasien.java

---
+++ Tugas_Modul 5/Tri wahyu_095/Package data/Pasien.java
@@ -0,0 +1,19 @@
+public class Pasien {
+
+    public String nama;
+    protected int umur;
+    public final String kodePasien = "PSN001";
+
+    // Constructor
+    public Pasien(String nama, int umur) {
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    public void tampilPasien() {
+        System.out.println("=== DATA PASIEN ===");
+        System.out.println("Kode Pasien : " + kodePasien);
+        System.out.println("Nama        : " + nama);
+        System.out.println("Umur        : " + umur);
+    }
+}
