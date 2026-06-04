468c87f1a84861e9c585e3e88f71f5071060f4fc
mahsunatulumamah32-art <mahsunatulumamah32@gmail.com>
2026-06-04 11:11:38 +0700

    Update Main.java

--- Tugas_Modul 5/Umamah_085/Main.java
+++ Tugas_Modul 5/Umamah_085/Main.java
@@ -1,1 +1,22 @@
-Main
+package Main;
+
+/*@author Taufiq*/
+import data.Mahasiswa;
+import kehadiran.MahasiswaAktif;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        Mahasiswa mhs = new Mahasiswa("Adit", 23, "Informatika", "Bangkalan");
+        mhs.tampilData();
+
+        System.out.println("\n------------------\n");
+
+        MahasiswaAktif aktif = new MahasiswaAktif("Umamah", 20, "Informatika", "Bangkalan");
+        aktif.aksesData();
+
+        System.out.println("\nAkses dari Main:");
+        System.out.println(mhs.nama);
+        System.out.println(mhs.getUmur());
+    }
+}   
