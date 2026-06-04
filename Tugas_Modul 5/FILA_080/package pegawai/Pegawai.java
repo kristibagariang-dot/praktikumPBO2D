5288c384073e9361be8788272f478935d6f094db
himakafilatul-09 <kafilatulhima5@gmail.com>
2026-06-04 15:16:19 +0700

    Add files via upload

--- Tugas_Modul 5/FILA_080/package pegawai/Pegawai.java
+++ Tugas_Modul 5/FILA_080/package pegawai/Pegawai.java
@@ -1,1 +1,19 @@
-bebas
+package pegawai;
+
+public class Pegawai {
+    public String nama;
+    protected String jabatan;
+    private double gaji;
+
+    public Pegawai(String nama, String jabatan, double gaji) {
+        this.nama = nama;
+        this.jabatan = jabatan;
+        this.gaji = gaji;
+    }
+
+    public void tampilInfo() {
+        System.out.println("Nama    : " + nama);
+        System.out.println("Jabatan : " + jabatan);
+        System.out.println("Gaji    : " + gaji);
+    }
+}
