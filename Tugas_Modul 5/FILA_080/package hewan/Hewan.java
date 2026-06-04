5ce68c69eb99c5365af1c2e7d8a7ee5f57a29e94
himakafilatul-09 <kafilatulhima5@gmail.com>
2026-06-04 15:10:35 +0700

    Add files via upload

--- Tugas_Modul 5/FILA_080/package hewan/Hewan.java
+++ Tugas_Modul 5/FILA_080/package hewan/Hewan.java
@@ -1,1 +1,19 @@
-bebasss
+package hewan;
+
+public class Hewan {
+    public String nama;
+    protected int umur;
+    private double berat;
+
+    public Hewan(String nama, int umur, double berat) {
+        this.nama = nama;
+        this.umur = umur;
+        this.berat = berat;
+    }
+
+    public void tampilInfo() {
+        System.out.println("Nama Hewan : " + nama);
+        System.out.println("Umur       : " + umur);
+        System.out.println("Berat      : " + berat);
+    }
+}
