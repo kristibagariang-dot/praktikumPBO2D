89085e65b69dde1d301f711b76fcfe6629cc2160
Nurulqomariyah1 <qomariamifda@gmail.com>
2026-06-04 14:03:16 +0700

    Add files via upload

--- Tugas_Modul 5/Riya_094/package tugas52/student.java
+++ Tugas_Modul 5/Riya_094/package tugas52/student.java
@@ -1,1 +1,19 @@
-bebaas
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas52;
+
+import tugas51.person;
+
+public class student extends person {
+
+    public void tampilData() {
+        System.out.println("Nama : " + nama);
+        System.out.println("Alamat : " + alamat);
+        tampilPublic();
+        tampilProtected();
+
+    }
+}
