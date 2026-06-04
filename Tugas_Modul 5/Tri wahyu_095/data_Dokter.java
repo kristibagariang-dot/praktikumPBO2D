df60493d55a4f2607ae0909601969b4f63bf7d00
Yuyanikko <triwahyufajarmalik496@gmail.com>
2026-06-04 18:40:01 +0700

    Create data_Dokter.java

---
+++ Tugas_Modul 5/Tri wahyu_095/data_Dokter.java
@@ -0,0 +1,18 @@
+package data;
+
+public class Dokter {
+
+    public String namaDokter;
+    protected String spesialis;
+
+    public Dokter(String namaDokter, String spesialis) {
+        this.namaDokter = namaDokter;
+        this.spesialis = spesialis;
+    }
+
+    public void tampilDokter() {
+        System.out.println("\n=== DATA DOKTER ===");
+        System.out.println("Nama Dokter : " + namaDokter);
+        System.out.println("Spesialis   : " + spesialis);
+    }
+}
