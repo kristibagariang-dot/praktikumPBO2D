ac57a6a92a94bc4fc7c2a5882d685cef0c5f13b8
mahsunatulumamah32-art <mahsunatulumamah32@gmail.com>
2026-06-04 11:08:27 +0700

    Update Mahasiswa.java

--- Tugas_Modul 5/Umamah_085/Mahasiswa.java
+++ Tugas_Modul 5/Umamah_085/Mahasiswa.java
@@ -1,1 +1,25 @@
-Main
+package data;
+
+/*@author Taufiq*/
+public class Mahasiswa {
+    public String nama;          
+    private int umur;            
+    protected String jurusan;    
+    String alamat;               
+
+    public Mahasiswa(String nama, int umur, String jurusan, String alamat) {
+        this.nama = nama;
+        this.umur = umur;
+        this.jurusan = jurusan;
+        this.alamat = alamat;
+    }
+    public int getUmur() {
+        return umur;
+    }
+    public void tampilData() {
+        System.out.println("Nama     : " + nama);
+        System.out.println("Umur     : " + umur);
+        System.out.println("Jurusan  : " + jurusan);
+        System.out.println("Alamat   : " + alamat);
+    }
+} 
