e13a9116d24cd812e7f41359e4b6983370fe3250
Nurulqomariyah1 <qomariamifda@gmail.com>
2026-06-04 14:04:50 +0700

    Add files via upload

--- Tugas_Modul 5/Riya_094/package tugas53/main.java
+++ Tugas_Modul 5/Riya_094/package tugas53/main.java
@@ -1,1 +1,23 @@
-babas
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas53;
+
+import tugas52.student;
+import tugas51.person;
+public class main {
+    public static void main(String[] args) {
+        person p = new person();
+        System.out.println(p.nama);
+        p.tampilPublic();
+        System.out.println("---------------");
+
+        student s = new student();
+        s.tampilData();
+
+        System.out.println("---------------");
+        p.aksesPrivate();
+    }
+}
