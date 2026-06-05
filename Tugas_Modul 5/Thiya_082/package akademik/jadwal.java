a26fc55bfcc6ead16c1dd7acecebd249bc8f73a9
fathiatulnoor-web <fathiatulnoor@gmail.com>
2026-06-05 22:11:11 +0700

    Create jadwal.java

---
+++ Tugas_Modul 5/Thiya_082/package akademik/jadwal.java
@@ -0,0 +1,17 @@
+package akademik;
+
+/**
+ *
+ * @author Noor Fathiatul Alea
+ */
+public class jadwal {
+    public String hari;
+    protected   String jam;
+    String ruang;
+    
+    public jadwal(String hari, String jam, String ruang){
+        this.hari = hari;
+        this.jam = jam;
+        this.ruang = ruang;
+    }
+}
