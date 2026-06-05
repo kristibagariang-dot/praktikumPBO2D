a33fc06466923b3fbdb728ab95dbea2f16011555
fathiatulnoor-web <fathiatulnoor@gmail.com>
2026-06-05 22:12:19 +0700

    Create dosen.java

---
+++ Tugas_Modul 5/Thiya_082/package kampus/dosen.java
@@ -0,0 +1,15 @@
+package kampus;
+
+/**
+ *
+ * @author Noor Fathiatul Alea
+ */
+public class dosen {
+    public String namaDosen;
+    protected String matkul;
+    
+    public dosen(String namaDosen, String matkul){
+        this.namaDosen = namaDosen;
+        this.matkul = matkul;
+    }
+}
