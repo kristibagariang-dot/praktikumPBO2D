a9a0847017bc2592f3d0fb9d11ab86f74c647b2c
auliarohmanraya-cpu <auliarohmanraya@gmail.com>
2026-06-07 23:37:16 +0700

    Create Dosen.java

---
+++ Tugas_Modul 5/Raya_092/Dosen.java
@@ -0,0 +1,10 @@
+package manusia;
+
+public class Dosen {
+
+    public String namaDosen = "Pak Budi";
+
+    public void tampilDosen() {
+        System.out.println("Nama Dosen : " + namaDosen);
+    }
+}
